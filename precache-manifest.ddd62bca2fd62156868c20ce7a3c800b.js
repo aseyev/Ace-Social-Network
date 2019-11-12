d05b212f69ccbe6d5ff92f24c013bb71b3ae92df
Anton <aseyev.1982@gmail.com>
2019-11-12 18:03:31 +0700

    Updates

---
+++ precache-manifest.ddd62bca2fd62156868c20ce7a3c800b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36bd33085a637bc9185998bc787cbd1",
+    "url": "/Ace-Social-Network/index.html"
+  },
+  {
+    "revision": "f54457965d4b508c9721",
+    "url": "/Ace-Social-Network/static/css/main.b4ccbd0c.chunk.css"
+  },
+  {
+    "revision": "7e03d4372588899c3ffb",
+    "url": "/Ace-Social-Network/static/js/2.8f8dc37f.chunk.js"
+  },
+  {
+    "revision": "f54457965d4b508c9721",
+    "url": "/Ace-Social-Network/static/js/main.02e00f2d.chunk.js"
+  },
+  {
+    "revision": "53285d4abef59c25889e",
+    "url": "/Ace-Social-Network/static/js/runtime-main.dce0841e.js"
+  }
+]);
